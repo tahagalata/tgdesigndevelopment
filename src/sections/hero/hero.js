ce924ad6b781c54ae688bba849ba539013ab5351
Taha Galata <tgalata2000@gmail.com>
2020-08-05 00:10:33 +0300

    added transition to buttons, seperated buttons to a seperate component

--- src/sections/hero/hero.js
+++ src/sections/hero/hero.js
@@ -19,13 +19,6 @@ class Hero extends Component {
             <h1>I'm Taha Galata</h1>
             <p className='hero-paragraph'>I am a web designer and web developer based in Çanakkale / Turkey. <span className='email-span'>Email me</span> if you think we should be working together on exciting projects.
             </p>
-            <div className='button-div'>
-              <button className='primary-button'>Get in Touch
-              </button>
-              
-              <button className='ghost-button'>See My Work
-              </button>
-            </div>
           </div>
           
           <div className='hero-photo'>
