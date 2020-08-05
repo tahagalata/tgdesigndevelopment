6d13ad38b333fe7daf14bc58fbe6a54d8c8fc2af
tahagalata <tgalata2000@gmail.com>
2020-08-05 23:02:57 +0300

    Fixed button component

---
+++ src/components/button/button-old.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './button.scss'
+
+class CustomButton extends Component {
+  render() {
+    return (
+      <div className='button-div'>
+        <div className='primary-button-div'>
+          <button className='primary-button'>Get in Touch
+          </button>
+          <div className='button-shadow'></div>
+        </div>
+        <div className='ghost-button-div'>
+          <button className='ghost-button'>See My Work
+          </button>
+          <div className='button-shadow'></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CustomButton;
--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,23 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './button.scss'
 
 class CustomButton extends Component {
   render() {
     return (
       <div className='button-div'>
-        <div className='primary-button-div'>
-          <button className='primary-button'>Get in Touch
-          </button>
-          <div className='button-shadow'></div>
-        </div>
-        <div className='ghost-button-div'>
-          <button className='ghost-button'>See My Work
-          </button>
-          <div className='button-shadow'></div>
-        </div>
+        <button 
+          className={this.props.type}>
+          {this.props.children}
+        </button>
+        <div className='button-shadow'/>
       </div>
     )
-  }
+  } 
 }
 
 export default CustomButton;
