18fc53c02371c7069ec6e07f6e1414fb087bc1ee
tahagalata <tgalata2000@gmail.com>
2020-08-07 21:32:25 +0300

    Changed folder structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react"
 import '../main.scss'
 import Navbar from '../components/navbar/navbar'
-import Hero from '../sections/hero/hero'
+import Hero from '../components/sections/hero/hero'
 
 export default function Home() {
   return (
