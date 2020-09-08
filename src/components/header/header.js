cfc30a22c5a0471cb9df91b3206ffca579a562ec
Taha Galata <tgalata2000@gmail.com>
2020-09-08 17:45:07 +0300

    hamburger menu on smaller screens made functional

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,17 +5,30 @@ import HamburgerMenuIcon from './hamburger.svg';
 import BrandLogo from '../brandLogo/brandLogo';
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navbarVisible: false
+    }
+  }
+
+  visibilityToggle() {
+    this.setState({navbarVisible: !this.state.navbarVisible})
+  }
+
   render() {
-    return (
+     return (
       <header>
         <div className='top-menu'>
           <BrandLogo screen='small'/>
-          <button className='menu-button'>
+          <button className='menu-button'
+          onClick={this.visibilityToggle.bind(this)}>
             <img src={HamburgerMenuIcon} alt='menu'></img>
           </button>
         </div>
+        
+        {this.state.navbarVisible && <Navbar activeSection={this.props.activeSection}/>}
 
-        <Navbar activeSection={this.props.activeSection}/>
       </header>
     )
   }
