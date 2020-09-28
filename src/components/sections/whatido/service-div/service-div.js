ea16148ee19b0b11011c1cb7f01ef37982076016
Taha Galata <tgalata2000@gmail.com>
2020-09-28 22:55:36 +0300

    Removed Web Maintenance and Copywriting Services

--- src/components/sections/whatido/service-div/service-div.js
+++ src/components/sections/whatido/service-div/service-div.js
@@ -5,8 +5,8 @@ class ServiceDiv extends Component {
   render() {
     return (
       <div className='service'>
-        <div className='service-icon-div'>
-          <img className='service-icon' src={require(`../assets/${this.props.type}.svg`)} alt={this.props.type}/>
+        <div className='service-icon'>
+          <img src={require(`../assets/${this.props.type}.svg`)} alt={this.props.type}/>
         </div>
 
         <h2 className='service-heading'>{this.props.heading}
--- src/components/sections/whatido/whatido.js
+++ src/components/sections/whatido/whatido.js
@@ -8,23 +8,14 @@ class WhatIDo extends Component {
       <section className='what-i-do section' id='whatido'>
         <h1 className='heading'>What I Do</h1>
         <div className='services-div'>
-          <ServiceDiv type='design' heading='Design'>
+          <ServiceDiv type='design' heading='UI & UX Design'>
             Whether it is a website or mobile app design, I deliver the perfect combination of unique visual aesthetics and joyful user experience.
           </ServiceDiv>
 
-          <ServiceDiv type='webdev'
-          heading='Website Development'>
+          <ServiceDiv type='webdev' heading='Web Development'>
             I deliver fully responsive, reliable and performant websites using the latest technologies like Webflow, React.js and Gatsby.js that fits my clients’ needs and budgets.
           </ServiceDiv>
 
-          <ServiceDiv type='webmaintenance' 
-          heading='Website Maintenance'>
-            I perform regular checks and maintenance on the websites I’ve built to ensure their performance and security so that my clients can rest assured.
-          </ServiceDiv>
-
-          <ServiceDiv type='copywriting' heading='Copywriting'>
-            I write powerful and persuasive copies that complement my clients products or businesses, with the websites I’ve built. Therefore I can deliver a complete package to my clients that is ready to publish.
-          </ServiceDiv>
         </div>
       </section>
     )
