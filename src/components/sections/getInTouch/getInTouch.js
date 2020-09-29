866573820d5da1a9b38952780f8d889d06533159
Taha Galata <tgalata2000@gmail.com>
2020-09-29 23:51:49 +0300

    contact me section made responsive

--- src/components/sections/getInTouch/getInTouch.js
+++ src/components/sections/getInTouch/getInTouch.js
@@ -7,25 +7,23 @@ class GetInTouch extends Component {
   render() {
     return (
       <section className='get-in-touch section' id='getintouch'>
-        <div className='main-container'>
-          <div className='at-div'>
-            <div className='at-sign'>
-              <img src={atsign} alt='@ sign'/>
-            </div>
-            <h1 className='at-heading'>Get in Touch</h1>
+        <div className='at-div'>
+          <div className='at-sign'>
+            <img src={atsign} alt='@ sign'/>
           </div>
+          <h1 className='at-heading'>Get in Touch</h1>
+        </div>
 
-          <div className='content'>
-            <p className='content-paragraph'>
-              If you need a modern and powerful website for your business, startup or yourself, I am available for work. You can email me directly at <span>
-                <a href='mailto: taha9alata@gmail.com' 
-                target='_blank' 
-                rel='noreferrer'
-                >taha9alata@gmail.com</a>
-              </span>.
-            </p>
-            <Button>Get in Touch</Button>
-          </div>
+        <div className='content'>
+          <p className='content-paragraph'>
+            If you need a modern and powerful website for your business, startup or yourself, I am available for work. You can email me directly at <span>
+              <a href='mailto: taha9alata@gmail.com' 
+              target='_blank' 
+              rel='noreferrer'
+              >taha9alata@gmail.com</a>
+            </span>.
+          </p>
+          <Button>Get in Touch</Button>
         </div>
       </section>
     )
