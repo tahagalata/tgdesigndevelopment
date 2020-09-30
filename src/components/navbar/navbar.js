c520255e14de14bbd12ee182be5dc773ac299479
Taha Galata <tgalata2000@gmail.com>
2020-09-30 18:02:17 +0300

    removed overlay in navbar, implemented a new way to hide navbar when clicked outside

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './navbar.scss';
 import NavbarLinks from './navbarLinks/navbarLinks';
 import SocialAndDarkMode from './socialAndDarkMode/socialAndDarkMode';
 import BrandLogo from '../brandLogo/brandLogo';
+import onClickOutside from "react-onclickoutside";
 
-export default function Navbar(props) {
-  return (
-    <>
+class Navbar extends Component {
+  handleClickOutside = () => {
+    this.props.visibilityToggle()
+  }
+
+  render() {
+    return (
       <nav className='navbar'>
         
         <BrandLogo screen='big'/>
 
-        <NavbarLinks activeSection={props.activeSection}/>
+        <NavbarLinks activeSection={this.props.activeSection}/>
         <SocialAndDarkMode/>
         
       </nav>
-
-      {props.isSmallScreen && 
-      <div className='overlay' 
-      onClick={props.visibilityToggle}
-      onKeyDown={props.visibilityToggle}
-      aria-hidden='true'
-      />}
-    </>
-  )
+    )
+  }
 }
+
+const EnhancedNavbar = onClickOutside(Navbar)
+
+class Container extends Component {
+  render(evt) {
+    const {activeSection, isSmallScreen, visibilityToggle} = this.props
+    return <EnhancedNavbar
+      activeSection = {activeSection}
+      isSmallScreen = {isSmallScreen}
+      visibilityToggle = {visibilityToggle}
+      disableOnClickOutside={!isSmallScreen}
+      outsideClickIgnoreClass={'menu-button'}
+    />
+  }
+}
+
+export default Container
